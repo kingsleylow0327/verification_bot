a413a82577c7e8593dae101c10f7727302a398dd
kingsleylow0327 <kingsleylow94@gmail.com>
2025-09-27 16:55:13 +0800

    add role logic

--- view/admid_verifiy_view.py
+++ view/admid_verifiy_view.py
@@ -1,7 +1,12 @@
+from sys import exception
 import discord
 import message as ms
+from logger import Logger
 from discord.ui import Button, View
 from discord.interactions import Interaction
+
+logger_mod = Logger("Admin Verify View")
+logger = logger_mod.get_logger()
 
 class AdminVerifyView(View):
     def __init__(self, config, user_info):
@@ -14,11 +19,29 @@ class AdminVerifyView(View):
     async def approve_button(self, interaction: discord.Interaction, button: Button):
         await self.send_thread(interaction.guild, self.user_info.player_id, ms.APPROVED)
         await interaction.message.edit(view=None)
+        try:
+            member = interaction.guild.get_member(self.user_info.player_id)
+            role = discord.utils.get(interaction.guild.roles, name="potato100")
+            await member.add_roles(role)
+        except Exception as e:
+            logger.error(f"Member with ID {self.user_info.player_id} failed to assigned role due to:")
+            logger.error(e)
+            await interaction.channel.send(f"Member with ID {self.user_info.player_id} failed to assigned role, please proceed with manual assign if this have no issue")
+
 
     @discord.ui.button(label="Reject", emoji="❌", style=discord.ButtonStyle.grey, custom_id="reject")
     async def reject_button(self, interaction: discord.Interaction, button: Button):
         await self.send_thread(interaction.guild, self.user_info.player_id, ms.REJECTED)
         await interaction.message.edit(view=None)
+        try:
+            member = interaction.guild.get_member(self.user_info.player_id)
+            role = discord.utils.get(interaction.guild.roles, name="potato100")
+            await member.remove_roles(role)
+        except Exception as e:
+            logger.error(f"Member with ID {self.user_info.player_id} failed to remove role due to:")
+            logger.error(e)
+            await interaction.channel.send(f"Member with ID {self.user_info.player_id} failed to remove role, please proceed with manual remove if this have no issue")
+
     
     async def send_thread(self, guild, member_id, message):
         channel = guild.get_channel(int(self.config.PUBLIC_THREAD_CHANNEL_ID))
